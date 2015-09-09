eeb67079eac8272ca972b3793dd47bc0c97a6956
Admin <admin@Mac-Admin.local>
2015-09-09 15:28:56 +0300

    Initial Commit

---
+++ HelloWo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWo
+//
+//  Created by Admin on 09.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWo
+//
+//  Created by Admin on 09.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
